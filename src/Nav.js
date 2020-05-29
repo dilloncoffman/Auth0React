9df4961280580eee8f0f4eb159c85a71b5b38346
Dillon Coffman <tug40394@temple.edu>
2020-05-29 09:47:06 -0400

    Styled app

--- src/Nav.js
+++ src/Nav.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/profile">Profile</Link></li>
-    </ul>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+      </ul>
+    </nav>
   )
 }
 export default Nav;
