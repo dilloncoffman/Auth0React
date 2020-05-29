434789c558a7c1deddd31922507572fc0033425d
Dillon Coffman <tug40394@temple.edu>
2020-05-29 11:57:20 -0400

    Implemented isAuthenticated function to determine if user is logged in or not and based on that, render a link to the profile page or a button to log in

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home(props) {
+  const { auth } = props;
+
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={props.auth.login}>Log In</button>
+      {auth.isAuthenticated() ? (
+        <Link to="/profile">View Profile</Link>
+      ) : (
+        <button onClick={auth.login} type="button">
+          Log In
+        </button>
+      )}
     </div>
   );
 }
